e2723652f850c09b1ce378d10baea6c290adc040
Gazaunga <Gazaunga@gmail.com>
2017-02-27 16:46:34 -0500

    Update boot.sh

--- bin/boot.sh
+++ bin/boot.sh
@@ -2,9 +2,12 @@
 cd ~/
 git clone https://github.com/nojhan/liquidprompt.git
 source liquidprompt/liquidprompt
-sudo apt-get -y install openbox
-sudo apt-get -y install xdg-user-dirs
+sudo apt-get -y install openbox obconf
+echo 'deb http://download.opensuse.org/repositories/home:/Head_on_a_Stick:/obmenu-generator/Debian_8.0/ /' > /etc/apt/sources.list.d/obmenu-generator.list
+sudo apt-get -y --allow-unauthenticated update
+sudo apt-get -y install xdg-user-dirs obmenu-generator
 xdg-user-dirs-update
+obmenu-generator -i -p -c
 sudo add-apt-repository ppa:rvm/smplayer
 sudo add-apt-repository ppa:mc3man/mpv-tests
 sudo add-apt-repository ppa:noobslab/apps
