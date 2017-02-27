fe97673d13589aea115facf036e7d236a0be021c
Gazaunga <Gazaunga@gmail.com>
2017-02-27 11:44:09 -0500

    Update boot.sh

--- bin/boot.sh
+++ bin/boot.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+cd ~/
+git clone https://github.com/nojhan/liquidprompt.git
+source liquidprompt/liquidprompt
+sudo apt-get -y install openbox
+sudo apt-get -y install xdg-user-dirs
+xdg-user-dirs-update
+sudo add-apt-repository ppa:rvm/smplayer
+sudo add-apt-repository ppa:mc3man/mpv-tests
+sudo add-apt-repository ppa:noobslab/apps
+sudo add-apt-repository ppa:nilarimogard/webupd8
+sudo apt-get -y --allow-unauthenticated update
 packagelist=packagelist
-
 xargs -a <(awk '/^\s*[^#]/' "$packagelist") -r -- sudo apt-get -y install
