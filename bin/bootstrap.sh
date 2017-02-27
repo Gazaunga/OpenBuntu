f621f6b7619d22d6178d5711c14f5de2a027877a
Gazaunga <Gazaunga@gmail.com>
2017-02-27 10:46:42 -0500

    Update bootstrap.sh

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,3 +1,21 @@
 cd ~/
 git clone https://github.com/nojhan/liquidprompt.git
 source liquidprompt/liquidprompt
+sudo apt-get -y install openbox
+sudo apt-get -y install xdg-user-dirs
+xdg-user-dirs-update
+sudo add-apt-repository ppa:rvm/smplayer
+sudo add-apt-repository ppa:mc3man/mpv-tests
+sudo add-apt-repository ppa:noobslab/apps
+
+
+
+sudo apt-get -y --allow-unauthenticated update
+cd ~/
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+sudo apt-get -y install libreoffice darktable smplayer smtube smplayer-themes smplayer-skins mpv clipgrab openshot covergloobus emacs python-pip
+sudo pip install rainbowstream glances ImageScraper howdoi
+sudo apt-get -qq update && sudo apt-get -qq dist-upgrade
+
+# http://www.omgubuntu.co.uk/2016/02/word-clock-conky-config-linux
+# https://www.box-look.org/p/1017851/
