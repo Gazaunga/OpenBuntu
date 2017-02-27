a17be24f0f3b7b4bccabb0a91db5adae91fd73a4
Gazaunga <Gazaunga@gmail.com>
2017-02-27 11:37:00 -0500

    Update bootstrap.sh

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -7,15 +7,16 @@ xdg-user-dirs-update
 sudo add-apt-repository ppa:rvm/smplayer
 sudo add-apt-repository ppa:mc3man/mpv-tests
 sudo add-apt-repository ppa:noobslab/apps
-
-
-
+sudo add-apt-repository ppa:nilarimogard/webupd8
 sudo apt-get -y --allow-unauthenticated update
+sudo apt-get -y --allow-unauthenticated install gdm texlive-full nautilus compton rofi moc texstudio arandr leafpad fontforge lxappearance rxvt-unicode-256color
 cd ~/
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
-sudo apt-get -y install libreoffice darktable smplayer smtube smplayer-themes smplayer-skins mpv clipgrab openshot covergloobus emacs python-pip
+sudo apt-get -y install libreoffice darktable smplayer smtube smplayer-themes smplayer-skins mpv clipgrab openshot covergloobus emacs python-pip albert
 sudo pip install rainbowstream glances ImageScraper howdoi
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys BA300B7755AFCFAE
+sudo add-apt-repository 'deb https://typora.io ./linux/'
+sudo apt-get -y --allow-unauthenticated update
 sudo apt-get -qq update && sudo apt-get -qq dist-upgrade
-
 # http://www.omgubuntu.co.uk/2016/02/word-clock-conky-config-linux
 # https://www.box-look.org/p/1017851/
