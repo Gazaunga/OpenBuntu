9fe9dabcf94dec03b50c86659f401f3722dcae4b
Gazaunga <Gazaunga@gmail.com>
2017-02-28 10:28:10 -0500

    Update boot.sh

--- bin/boot.sh
+++ bin/boot.sh
@@ -21,4 +21,5 @@ sudo pip install rainbowstream glances ImageScraper howdoi
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys BA300B7755AFCFAE
 sudo add-apt-repository 'deb https://typora.io ./linux/'
 sudo apt-get -y --allow-unauthenticated update
+xmodmap -pke > ~/.xmodmap
 openbox --reconfigure
